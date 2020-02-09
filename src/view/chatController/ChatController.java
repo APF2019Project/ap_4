7adeacb0c7f315296bca7bd08237b48a1618d579
komeilyahyazade <komeilyahyazade@gmail.com>
2020-02-09 20:31:42 +0330

    config file added

--- src/view/chatController/ChatController.java
+++ src/view/chatController/ChatController.java
@@ -1,5 +1,50 @@
 package view.chatController;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class ChatController {
 
+    @FXML
+    private ListView<String> accountList;
+
+    @FXML
+    private ListView<String> messageList;
+
+    @FXML
+    private TextField textMessage;
+
+    @FXML
+    private ImageView targetImage;
+
+    @FXML
+    private ImageView imageMessage;
+
+    @FXML
+    public void chooseImage() {
+
+    }
+
+    @FXML
+    public void reply() {
+        String text = messageList.getSelectionModel().getSelectedItem() ;
+
+    }
+
+    @FXML
+    public void replyMessage() {
+
+    }
+
+    @FXML
+    public void send() {
+
+    }
+
+    @FXML
+ public void startChat() {
+
+    }
 }
